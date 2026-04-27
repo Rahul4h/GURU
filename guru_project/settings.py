e29722948bfdee710c1442fafb984701e0caa7da
Rahul4h <rahul255gh68@gmail.com>
2026-04-27 12:19:05 +0600

    deployed version

--- guru_project/settings.py
+++ guru_project/settings.py
@@ -175,13 +175,3 @@ X_FRAME_OPTIONS = "DENY"
 
 
 
-if os.getenv("DJANGO_SUPERUSER_USERNAME"):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    
-    if not User.objects.filter(username=os.getenv("DJANGO_SUPERUSER_USERNAME")).exists():
-        User.objects.create_superuser(
-            username=os.getenv("DJANGO_SUPERUSER_USERNAME"),
-            email=os.getenv("DJANGO_SUPERUSER_EMAIL"),
-            password=os.getenv("DJANGO_SUPERUSER_PASSWORD")
-        )
